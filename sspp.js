3ec8a8008d7ded2daa640a1bb9298d37f85451e4
Lance Hunter <lancehunter@gmail.com>
2017-11-08 15:19:43 -0600

    Creating first object

---
+++ sspp.js
@@ -0,0 +1,10 @@
+function reportLostDog(dog, phone, seen, size, color) {
+  let arr = [];
+  let obj = {};
+  obj.dogName = dog;
+  obj.ownerPhone = phone;
+  obj.zipSeen = seen;
+  obj.dogSize = size;
+  obj.dogColor = color;
+  return obj;
+}

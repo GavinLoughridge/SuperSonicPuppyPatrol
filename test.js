0558da6de5b8eaf2a264bfce3322f07e3e75d94e
Jason Amador <jason.amador@gmail.com>
2017-11-08 14:36:34 -0600

    some tests

--- test.js
+++ test.js
@@ -3,6 +3,10 @@ const SSPP = require('../sspp');
 
 describe("report lost dog", function () {
   it("returns an object", function () {
-     expect(SSPP.reportLostDog('name', '1111111111', '11111', 'size', 'color')).to.be.an('array');
+   expect(SSPP.reportLostDog('name', '1111111111', '11111', 'size', 'color')).to.be.an('array');
   });
+
+  it("has the right keys", function() {
+     expect(SSPP.reportLostDog('name', '1111111111', '11111', 'size', 'color')).to.have.all.keys('dogName', 'ownerPhone', 'zipSeen', 'dogSize', 'dogColor');
+   });
 })
